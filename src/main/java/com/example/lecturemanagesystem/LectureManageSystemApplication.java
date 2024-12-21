8bb4331da6d621e4664b5f0564921b21a56cf915
ynyejn <yeounshi@gmail.com>
2024-12-22 01:32:07 +0900

    Init : initial commit

---
+++ src/main/java/com/example/lecturemanagesystem/LectureManageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.lecturemanagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureManageSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureManageSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lecturemanagesystem/LectureManageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lecturemanagesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureManageSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
